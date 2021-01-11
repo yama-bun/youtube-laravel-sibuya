1da171db6bfa327b352648f004becf388ac58e8d
yama-bun <baseball8296@gmail.com>
2021-01-11 22:18:46 +0900

    test

---
+++ blog/laravel/app/Http/Controllers/BlogController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    //ブログ一覧を表示する
+    public function showList()
+    {
+        return view('blog.list');
+    }
+}
---
+++ blog/laravel/routes/web.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::get('/', 'BlogController@showList')->name('blogs');
